fa12b5773d9a687dae620979ca912fbaaf252ea9
Jelena Tasic <jelenatasicsd@gmail.com>
2022-01-09 12:14:59 +0100

    Initial Commit

---
+++ NetflixUdemy/NetflixUdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUdemyApp.swift
+//  NetflixUdemy
+//
+//  Created by Jelena Tasic on 9.1.22..
+//
+
+import SwiftUI
+
+@main
+struct NetflixUdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
